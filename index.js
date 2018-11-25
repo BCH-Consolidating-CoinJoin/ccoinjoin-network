2617740d5fb3601b28508b05d68cda316e1697a4
Chris Troutner <chris.troutner@gmail.com>
2018-11-25 09:25:10 -0800

    created class and bootstrap ipfs arrays

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 /*
+  An npm library that creates the Consolidating Coinjoin Network (ccNet) class.
+  This class object is used to join the peer-to-peer network.
 */
 
 "use strict";
 
-module.exports = {
-  helloWorld
-};
+const ipfsData = require('./data/ipfs-bootstrap.json')
+const ccoinjoinData = require('./data/ccoinjoin-bootstrap.json')
 
-function helloWorld() {
-  console.log(`Hello semantic release!`);
+const util = require("util");
+util.inspect.defaultOptions = {depth: 1};
+
+class CCNet {
+  constructor() {
+    //console.log(`ipfsBootstrap: ${util.inspect(ipfsBootstrap)}`)
+    this.bootstrap = ipfsData.ipfsBootstrap.concat(ccoinjoinData.ccoinjoinBootstrap)
+  }
 }
+
+module.exports = CCNet;
--- test/unit/a01-ccNet.js
+++ test/unit/a01-ccNet.js
@@ -5,13 +5,14 @@
 "use strict"
 
 const assert = require('chai').assert
-const hello = require('../../index')
+const CCNet = require('../../index')
 
 describe('Function', () => {
   describe('Feature', () => {
     it('should run a test', () => {
-      hello.helloWorld()
-      assert.equal(true,true)
+      const ccNet = new CCNet()
+
+      assert.isArray(ccNet.bootstrap)
     })
   })
 })
