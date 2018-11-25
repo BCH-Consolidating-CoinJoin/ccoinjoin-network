769dc8b94e6282bad9fadf25794fb99fffc11c30
Chris Troutner <chris.troutner@gmail.com>
2018-11-24 20:54:03 -0800

    Adding basic scaffolding

---
+++ test/unit/a01-first.js
@@ -0,0 +1,15 @@
+/*
+*/
+
+
+"use strict"
+
+const assert = require('chai').assert
+
+describe('Function', () => {
+  describe('Feature', () => {
+    it('should run a test', () => {
+      assert.equal(true,true)
+    })
+  })
+})
