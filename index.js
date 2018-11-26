d5db1477fe303feb42da369f9f336886a69ba851
Chris Troutner <chris.troutner@gmail.com>
2018-11-25 18:31:17 -0800

    Added connectToIPFS

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 
 "use strict";
 
+const IPFS = require('ipfs')
+const OrbitDB = require('orbit-db')
+
+// Load locally saved data.
 const ipfsData = require('./data/ipfs-bootstrap.json')
 const ccoinjoinData = require('./data/ccoinjoin-bootstrap.json')
 
@@ -14,8 +18,60 @@ util.inspect.defaultOptions = {depth: 1};
 class CCNet {
   // Connect to the IPFS network and load the DB.
   constructor() {
-    //console.log(`ipfsBootstrap: ${util.inspect(ipfsBootstrap)}`)
     this.bootstrap = ipfsData.ipfsBootstrap.concat(ccoinjoinData.ccoinjoinBootstrap)
+    this.ipfs = {} // Will contain an instance of IPFS.
+    this.db = {} // Will contain an instance of the OrbitDB.
+  }
+
+  // Connect to the IPFS network
+  async connectToIPFS() {
+    // OrbitDB uses Pubsub which is an experimental feature and needs to be
+    // turned on manually.
+    const ipfsOptions = {
+      start: true,
+      EXPERIMENTAL: {
+        pubsub: true
+      },
+      config: {
+        // Override default bootstrap list with save list of bootstrap peers.
+        Bootstrap: this.bootstrap
+      }
+    }
+
+    const ipfs = new IPFS(ipfsOptions)
+
+    ipfs.on('error', err => console.error(err))
+
+    // Once IPFS is ready, initialize the DB.
+    ipfs.on('ready', async () => {
+      console.log('IPFS is ready.')
+
+      console.log(`ipfs: ${util.inspect(ipfs)}`)
+
+      await this.openDB(ipfs)
+    })
+  }
+
+  // Called by the ipfs on.ready event. Opens the DB and loads locally saved data.
+  async openDB(ipfs) {
+    const access = {
+      // Give write access to everyone
+      write: ['*']
+    }
+
+    // Create OrbitDB instance
+    const orbitdb = new OrbitDB(ipfs)
+
+    // Instantiate the ccoinjoin database.
+    const db = await orbitdb.eventlog('ccoinjoin', access)
+
+    // Load any saved state from disk.
+    await db.load()
+
+    // React to DB update events.
+    db.events.on('replicated', () => {
+      console.log(`replication event fired`)
+    })
   }
 
   //Update List - Update the list of IPFS bootstrap servers, and validate the list
--- test/unit/a01-ccNet.js
+++ test/unit/a01-ccNet.js
@@ -7,12 +7,20 @@
 const assert = require('chai').assert
 const CCNet = require('../../index')
 
-describe('Function', () => {
-  describe('Feature', () => {
-    it('should run a test', () => {
+describe('ccNet', () => {
+  describe('constructor()', () => {
+    it('should instantiate the class', () => {
       const ccNet = new CCNet()
 
       assert.isArray(ccNet.bootstrap)
+    })
+  })
+
+  describe('connectToIPFS()', () => {
+    it('should connect to IPFS', () => {
+      const ccNet = new CCNet()
+
+      ccNet.connectToIPFS()
     })
   })
 })
